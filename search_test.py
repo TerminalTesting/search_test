1c4e4683a8ba58a25b2f374004709cd102994dcd
TerminalTesting <testing@terminal.ru>
2014-10-22 18:28:14 +0600

    forget import some classes

--- search_test.py
+++ search_test.py
@@ -5,6 +5,9 @@ import sys
 import os
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class SearchTest(unittest.TestCase):
